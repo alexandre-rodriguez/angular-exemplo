c531dc93a8f1c56dbb27711fd81ccbc1d6640b01
Alexandre Rodriguez <alexandre.rodriguez@serpro.gov.br>
2022-02-09 09:11:27 -0300

    corrigindo o toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BootstrapModalModule } from './bootstrap-modal/bootstrap-modal.module';
-import { BotaoMidiasSociaisModule } from './botao-midias-sociais/botao-midias-sociais.module';
-import { NgxToastrModule } from './ngx-toastr/ngx-toastr.module';
-import { InternacionalizacaoModule } from './internacionalizacao/internacionalizacao.module';
-
-
-import { RecaptchaV2Module } from './recaptchas/recaptcha-v2/recaptcha-v2.module';
-import { RecaptchaV3InternalModule } from './recaptchas/recaptcha-v3/recaptcha-v3.module';
-import { LoggerInternalModule } from './logger/logger.module';
-import { HighcartsModule } from './highcarts/highcarts.module';
-import { SpinnerModule } from './spinner/spinner.module';
-import { FormValidationModule } from './form-validation/form-validation.module';
-
-
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -26,6 +13,12 @@ import { FormValidationModule } from './form-validation/form-validation.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
+    ToastrModule.forRoot({  // ngx-toastr
+      timeOut: 50000, // 5 seconds
+      closeButton: true,
+      progressBar: true,
+    }),
 
     AppRoutingModule,
   ],
--- src/app/ngx-toastr/ngx-toastr.module.ts
+++ src/app/ngx-toastr/ngx-toastr.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxToastrRoutingModule } from './ngx-toastr-routing.module';
-import { ToastrModule } from 'ngx-toastr';
-
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     NgxToastrRoutingModule,
-    ToastrModule.forRoot({  // ngx-toastr
-      timeOut: 50000, // 5 seconds
-      closeButton: true,
-      progressBar: true,
-    }),
   ]
 })
 export class NgxToastrModule { }
